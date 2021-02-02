3045637ea46932306496af766997f499a16b29e8
kakiman <aurelien.duriez@gmail.com>
2021-02-02 16:07:40 +0100

    chore: add auth and axios support

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,6 +17,11 @@ export default {
     },
     plugins:[ '~/plugins/maps.client', '~/plugins/dataApi' ],
   modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios',
+    '@nuxtjs/proxy',
+    // Doc: https://auth.nuxtjs.org/
+    '@nuxtjs/auth-next',
     // Doc: https://nuxt-community.github.io/nuxt-i18n/
     'nuxt-i18n',
     // Doc: https://github.com/nuxt-community/dotenv-module
