7b52046028220a944af2b406cf42ffa87193769e
kakiman <aurelien.duriez@gmail.com>
2021-02-08 18:08:11 +0100

    refactor: split default layout in 4 files

---
+++ layouts/default.ts
@@ -0,0 +1,27 @@
+import { Vue, Component, Prop, Ref } from 'nuxt-property-decorator'
+
+@Component({
+  name: 'default'
+  //middleware: ['auth']
+})
+export default class Default extends Vue {
+  @Ref() readonly citySearch!: HTMLInputElement
+
+  mounted() {
+    this.$maps.makeAutoComplete(this.citySearch)
+  }
+
+  changed(event: { detail: any }) {
+    const place = event.detail
+    if (!place.geometry) return
+
+    this.$router.push({
+      name: 'search',
+      query: {
+        lat: place.geometry.location.lat(),
+        lng: place.geometry.location.lng(),
+        label: this.citySearch.value
+      }
+    })
+  }
+}
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,31 +1,3 @@
-<template>
-  <div>
-    <header style="background-color:#eee;">
-      <nuxt-link to="/">Home</nuxt-link>
-      <input ref="citySearch" type="text" v-on:changed="changed" />
-    </header>
-    <nuxt />
-  </div>
-</template>
-<script>
-export default {
-  mounted() {
-    this.$maps.makeAutoComplete(this.$refs.citySearch)
-  },
-  methods: {
-    changed(event) {
-      const place = event.detail
-      if (!place.geometry) return
-
-      this.$router.push({
-        name: 'search',
-        query: {
-          lat: place.geometry.location.lat(),
-          lng: place.geometry.location.lng(),
-          label: this.$refs.citySearch.value
-        }
-      })
-    }
-  }
-}
-</script>
+<template lang="html" src="./default.html"></template>
+<script lang="ts" src="./default.ts" name="default"></script>
+<style lang="scss" src="./default.scss" scoped></style>
